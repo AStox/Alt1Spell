a877468623308c98071a1e3e8f62b8ed35bfb00d
Adam Stox <adamstox@protonmail.com>
2024-06-26 16:06:54 -0400

    update spell

--- src/Spell.sol
+++ src/Spell.sol
@@ -7,6 +7,14 @@ interface PileLike {
     function file(bytes32 what, uint256 rate, uint256 value) external;
     function loanRates(uint256 loan) external view returns (uint256);
     function rates(uint256 loanRate) external view returns (Rate memory);
+    function drip(uint256 rate) external;
+}
+
+interface FeedLike {
+    function calcUpdateNAV() external returns (uint256);
+    function update(bytes32 nftID_, uint value, uint risk_) external;
+    function nftID(uint loan) external view returns (bytes32);
+    function risk(bytes32 nft_) external view returns (uint);
 }
 
 struct Rate {
@@ -21,6 +29,7 @@ contract Spell {
     bool public executed = false;
     uint256[18] public loans = [25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,7,6,5];
     address public alt1Pile = 0xE18AAB16cC26EB23740D72875e0C6b52cEbb46b3;
+    address public alt1NavFeed = address(0x6fb02533B264d103B84d8f13D11a4865EC96307a);
 
     function execute() public {
         require(!executed, "Already executed");
@@ -36,6 +45,12 @@ contract Spell {
             Rate memory rate = PileLike(alt1Pile).rates(loanRate);
             PileLike(alt1Pile).file("rate", loanRate, rate.ratePerSecond);
             PileLike(alt1Pile).decDebt(loan, debt);
+            bytes32 nftID = FeedLike(alt1NavFeed).nftID(loan);
+            uint256 risk = FeedLike(alt1NavFeed).risk(nftID);
+            risk += 1;
+            if (risk > 4) risk = 1;
+            FeedLike(alt1NavFeed).update(nftID, 0, risk);
+            PileLike(alt1Pile).drip(loanRate);
         }
     }
 }
--- test/Spell.t.sol
+++ test/Spell.t.sol
@@ -8,17 +8,36 @@ interface RootLike {
     function relyContract(address target, address user) external;
 }
 
+interface FeedLike {
+    function currentNAV() external view returns (uint256);
+    function nftValues(bytes32 nftID) external view returns (uint256);
+    function nftID(uint loan) external view returns (bytes32);
+}
+
 contract SpellTest is Test {
     Spell public spell;
     address public root = address(0xF96F18F2c70b57Ec864cC0C8b828450b82Ff63e3);
     address public pile = address(0xE18AAB16cC26EB23740D72875e0C6b52cEbb46b3);
+    address public navFeed = address(0x6fb02533B264d103B84d8f13D11a4865EC96307a);
     address public multisig = address(0xf3BceA7494D8f3ac21585CA4b0E52aa175c24C25);
 
     function setUp() public {
         spell = new Spell();
-        vm.prank(multisig);
+        vm.startPrank(multisig);
         RootLike(root).relyContract(pile, address(spell));
+        RootLike(root).relyContract(navFeed, address(spell));
+        vm.stopPrank();
     }
+
+    // function test_executingTwice_fails() public {
+    //     spell.execute();
+    //     vm.expectRevert("Already executed");
+    //     spell.execute();
+    // }
+
+    // function test_navFeed() public {
+    //     assertEq(FeedLike(navFeed).currentNAV(), 0);
+    // }
 
     function test_execute() public {
         spell.execute();
@@ -26,12 +45,10 @@ contract SpellTest is Test {
             uint256 loan = spell.loans(i);
             uint256 debt = PileLike(pile).debt(loan);
             assertEq(debt, 0);
+            bytes32 nftID = FeedLike(navFeed).nftID(loan);
+            assertEq(FeedLike(navFeed).nftValues(nftID), 0);
         }
+        assertEq(FeedLike(navFeed).currentNAV(), 0);
     }
 
-    function test_executingTwice_fails() public {
-        spell.execute();
-        vm.expectRevert("Already executed");
-        spell.execute();
-    }
 }
